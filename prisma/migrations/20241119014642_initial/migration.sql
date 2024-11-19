4b595937b1de6c559ab8fc0bdd2601b937d8a031
Ahmed Keifala <ahmedkeifala1@gmail.com>
2024-11-19 01:48:10 +0000

    Database config

---
+++ prisma/migrations/20241119014642_initial/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "fullName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'created',
+    "emailVerifiedAt" DATE,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
